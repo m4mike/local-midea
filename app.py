f60737516897b5f49d9264b56b9d88ba1526b3e3
michael bentein <michael.bentein@gmail.com>
2021-05-14 23:05:35 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import socket
+import os
+
+FAKE_CLOUD_IP = '0.0.0.0'
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+sock.bind((FAKE_CLOUD_IP, 443))
+sock.listen(5)
+while True:  
+    connection,address = sock.accept()  
+    buf = connection.recv(1024)
+    print(buf)
+    connection.send(buf)
